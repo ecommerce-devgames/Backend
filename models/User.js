1454bb211ab9fa9c91da8c59948a820031b9c65a
sergiogenes <genessergio@gmail.com>
2023-03-07 16:43:44 -0300

    Model User done

---
+++ models/User.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+
+const db = require("../db");
+
+class User extends Model {
+  hash(password, hash) {
+    return bcrypt.hash(password, hash);
+  }
+
+  validatePassword(password) {
+    return this.hash(password, this.salt).then(
+      (hash) => hash === this.password
+    );
+  }
+}
+
+User.init(
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    salt: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: "user",
+  }
+);
+
+User.addHook("beforeValidate", (user) => {
+  user.salt = bcrypt.genSaltSync(8);
+  return user
+    .hash(user.password, user.salt)
+    .then((hash) => (user.password = hash));
+});
+
+module.exports = User;
