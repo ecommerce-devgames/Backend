4dcbfc537f38ee7df9f2c8e8d19963991c3739e6
Fabio-Alessandro <fabioalessandrotr@gmail.com>
2023-03-08 09:17:57 -0600

    Auth functions done.

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const db = require("./db");
+const cookieParser = require ('cookie-parser');
+const routes = require ("./routes");
 const { User } = require("./models");
 const PORT = 3001;
 const server = express();
+
+server.use (cookieParser ());
+server.use ("/api", routes);
 
 db.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
---
+++ utils/token.js
@@ -0,0 +1,8 @@
+const jwt = require ("jsonwebtoken");
+
+const SECRET = "A1Z9";
+
+const generateToken = payload => jwt.sign (payload, SECRET, { expiresIn: "2h "});
+const verifyToken = token => jwt.verify (token, SECRET);
+
+module.exports = { generateToken, verifyToken }
