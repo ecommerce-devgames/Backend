6c56f3f6139532aa0a1878ab4ed508104970debf
sergiogenes <genessergio@gmail.com>
2023-03-08 12:09:31 -0300

    Developer model done, unrelated to other tables

--- models/index.js
+++ models/index.js
@@ -3,5 +3,6 @@ const Game = require("./Game");
 const Platform = require("./Platform");
 const Genres = require("./Genres");
 const Tag = require("./Tag");
+const Developer = require("./Developer");
 
-module.exports = { User, Game, Platform, Genres, Tag };
+module.exports = { User, Game, Platform, Genres, Tag, Developer };
