52110726d5af2b2a7f2cdb3045d451b08a5da933
sergiogenes <genessergio@gmail.com>
2023-03-08 11:09:38 -0300

    Game model done, unrelated to other tables

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const db = require("./db");
-const { User } = require("./models");
+const { User, Game } = require("./models");
 const PORT = 3001;
 const server = express();
 
-db.sync({ force: true }).then(() => {
+db.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log("Server listening at port: " + PORT);
   });
