16c5748063b0a8998742b8d9dcb36714203e35f2
sergiogenes <genessergio@gmail.com>
2023-03-09 12:46:27 -0300

    game relationships done

--- models/index.js
+++ models/index.js
@@ -8,10 +8,33 @@ const Library = require("./Library");
 const Transaction = require("./Transaction");
 const Cart = require("./Cart");
 
+// Relationship Cart-User
 Cart.belongsTo(User);
 User.hasOne(Cart);
+
+// Relationship Cart-Game
 Game.belongsToMany(Cart, { through: "cartItem" });
 Cart.belongsToMany(Game, { through: "cartItem" });
+
+// Relationship Game-Developer
+
+Game.belongsToMany(Developer, { through: "game_developer" });
+Developer.belongsToMany(Game, { through: "game_developer" });
+
+// Relationship Game-Platform
+
+Game.belongsToMany(Platform, { through: "game_platform" });
+Platform.belongsToMany(Game, { through: "game_platform" });
+
+// Relationship Game-Genres
+
+Game.belongsToMany(Genres, { through: "game_genres" });
+Genres.belongsToMany(Game, { through: "game_genres" });
+
+// Relationship Game-Tag
+
+Game.belongsToMany(Tag, { through: "game_tag" });
+Tag.belongsToMany(Game, { through: "game_tag" });
 
 module.exports = {
   User,
