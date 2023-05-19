5f5c8c516e031f7da4031d6c7f3c6b9bade584f0
sergiogenes <genessergio@gmail.com>
2023-05-19 14:44:13 -0300

    configurando un pool en la conexión a la db

--- db/index.js
+++ db/index.js
@@ -11,6 +11,12 @@ const DB_URI = process.env.DB_URI;
 const db = new Sequelize(DB_URI, {
   dialectModule: require("pg"),
   logging: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 module.exports = db;
